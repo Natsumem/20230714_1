796ada8eb0f672285cb719fd2c35153f7b6dc633
Natsumem <go2008nn@yahoo.co.jp>
2023-07-14 17:31:23 +0900

    Selenumサンプルファイルの追加

---
+++ Selenum_Sample_Tokizawa.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome import service as fs
+
+CHROMEDRIVER = "C:\data\etc\chromedriver.exe"
+
+# 検索ワードを入力する
+keyword = input("検索ワードを入力してください：")
+
+# ドライバー指定でChromeブラウザを開く
+chrome_service = fs.Service(executable_path=CHROMEDRIVER)
+browser = webdriver.Chrome(service=chrome_service)
+ 
+# Googleアクセス
+browser.get('https://www.google.com/')
+
+# サイト内検索ボックスのnameをセット
+search_box = browser.find_element(By.NAME, "q")
+
+# 入力した検索ワードをサイト内検索に出力
+search_box.send_keys(keyword)
+
+# 検索実行
+search_box.send_keys(Keys.RETURN)
